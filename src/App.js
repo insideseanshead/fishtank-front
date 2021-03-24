8d6ba1342cac036fc46e57c313585062dab7cc0d
Sean Morgan <insideseanshead@gmail.com>
2021-03-24 12:42:05 -0700

    added else to log in state where if fails it deletes token and resets state

--- src/App.js
+++ src/App.js
@@ -1,64 +1,87 @@
-import React, {useState, useEffect} from 'react';
-import API from './utils/API'
+import React, { useState, useEffect } from "react";
+import API from "./utils/API";
 
 function App() {
   const [loginFormState, setloginFormState] = useState({
-    email:'',
-    password:''
-  })
+    email: "",
+    password: "",
+  });
 
-  const [profileState,setProfileState]=useState({
-    name:'',
-    email:"",
-    tanks:[],
-    isLoggedIn:false
-  })
+  const [profileState, setProfileState] = useState({
+    name: "",
+    email: "",
+    tanks: [],
+    isLoggedIn: false,
+  });
 
-  useEffect(()=>{
+  useEffect(() => {
     const token = localStorage.getItem("token");
-    API.getProfile(token).then(profileData=>{
-      if(profileData){
+    API.getProfile(token).then((profileData) => {
+      if (profileData) {
         setProfileState({
-          name:profileData.name,
-          email:profileData.email,
-          tanks:profileData.Tanks,
-          isLoggedIn:true
-        })
+          name: profileData.name,
+          email: profileData.email,
+          tanks: profileData.Tanks,
+          isLoggedIn: true,
+        });
+      } else {
+        localStorage.removeItem("token");
+        setProfileState({
+          name: "",
+          email: "",
+          tanks: [],
+          isLoggedIn: false,
+        });
       }
-    })
-  },[])
+    });
+  }, []);
 
-  const inputChange = event =>{
-    const {name,value}=event.target;
+  const inputChange = (event) => {
+    const { name, value } = event.target;
     setloginFormState({
       ...loginFormState,
-      [name]:value
-    })
-  }
+      [name]: value,
+    });
+  };
 
-  const formSubmit = event=>{
+  const formSubmit = (event) => {
     event.preventDefault();
-    API.login(loginFormState).then(newToken=>{
-      localStorage.setItem("token",newToken.token)
-      API.getProfile(newToken.token).then(profileData=>{
+    API.login(loginFormState).then((newToken) => {
+      localStorage.setItem("token", newToken.token);
+      API.getProfile(newToken.token).then((profileData) => {
         setProfileState({
-          name:profileData.name,
-          email:profileData.email,
-          tanks:profileData.Tanks,
-          isLoggedIn:true
-        })
-      })
-    })
-  }
+          name: profileData.name,
+          email: profileData.email,
+          tanks: profileData.Tanks,
+          isLoggedIn: true,
+        });
+      });
+    });
+  };
 
   return (
     <div className="App">
       <form onSubmit={formSubmit}>
-        <input onChange = {inputChange} value={loginFormState.email} type='text' name='email' placeholder='email'></input>
-        <input onChange = {inputChange} value={loginFormState.password} type='password' name='password'></input>
-        <input type='submit' value='login'></input>
+        <input
+          onChange={inputChange}
+          value={loginFormState.email}
+          type="text"
+          name="email"
+          placeholder="email"
+        ></input>
+        <input
+          onChange={inputChange}
+          value={loginFormState.password}
+          type="password"
+          name="password"
+        ></input>
+        <input type="submit" value="login"></input>
       </form>
-      {profileState.isLoggedIn?profileState.tanks.map(tankObj=><p>{tankObj.name}</p>):<h1>Log in to see your tanks</h1>}
+      {profileState.isLoggedIn ? (
+        profileState.tanks.map((tankObj) => <p>{tankObj.name}</p>)
+      ) : (
+        <h1>Log in to see your tanks</h1>
+      )}
     </div>
   );
 }
