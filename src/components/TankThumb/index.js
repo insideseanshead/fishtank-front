f5e1bfcfc08fb09fba32be4f3b56b4adbf221aa7
Sean Morgan <insideseanshead@gmail.com>
2021-03-27 22:55:11 -0700

    created fish component

--- src/components/TankThumb/index.js
+++ src/components/TankThumb/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import FishThumbnail from '../fishThumbnail'
 import "./style.css"
 
 const TankThumbnail = (props) => {
     return (
         <div className="TankThumbnail">
             <h3>{props.name}</h3>
-            {props.fish.map((fishObj)=><p>{fishObj.name}</p>)}
+            {props.fish.map((fishObj)=><FishThumbnail name={fishObj.name} color={fishObj.color} width={fishObj.width} />)}
         </div>
     )
 }
